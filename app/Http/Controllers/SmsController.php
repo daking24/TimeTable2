1047bf8f0dd54d9ad748eec51c4da8c7d1a381ae
daking24 <daking3968@outlook.com>
2021-11-11 06:35:59 +0100

    SMS

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -4,16 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Nexmo\Laravel\Facade\Nexmo;
+use App\Lesson;
 
 class SmsController extends Controller
 {
     public function index()
     {
-        $sms = Nexmo::message()->send([
-            'to'   => '234reciever',
-            'from' => 'sender',
-            'text' => 'info',
-        ]);
+        $weekdays=Lesson::WEEK_DAYS;
+        $today=date('l');
+        $lessons=Lesson::where('week_day',$today)->with('teacher', 'class')->get();
+        $current_time=date('H:i:s', strtotime(now()));
+        $thirty_minutes_from_now=date('H:i:s', strtotime(now() . '+30 minutes'));
+        if ($lessons->count()>0) {
+            foreach ($lessons as $lesson) {
+
+                if ($lesson->start_time==$thirty_minutes_from_now) {
+                    Nexmo::message()->send([
+                        'to'   => $lesson->teacher->phone,
+                        'from' => 'Time Table',
+                        'text' => 'Time to prepare for your lesson at'.$lesson->class->name.' class',
+                    ]);
+
+                } elseif ($lesson->start_time==$current_time) {
+                    Nexmo::message()->send([
+                        'to'   => $lesson->teacher->phone,
+                        'from' => 'Time Table',
+                        'text' => 'Time to start your lesson at'.$lesson->class->name.' class',
+                    ]);
+                }
+            }
+        }
 
 
     }
